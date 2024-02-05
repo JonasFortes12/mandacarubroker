04e9bc3e8007d992b651d8c0e6ab65d86b70be1b
JonasFortes12 <jonascforte@hotmail.com>
2024-02-05 19:09:28 -0300

    Chore: Refact the importations to explicit mode
    Code refactored to explicit importation, without using * for imports all modules in library

--- src/main/java/com/mandacarubroker/service/StockService.java
+++ src/main/java/com/mandacarubroker/service/StockService.java
@@ -3,7 +3,13 @@ package com.mandacarubroker.service;
 import com.mandacarubroker.domain.stock.RequestStockDTO;
 import com.mandacarubroker.domain.stock.Stock;
 import com.mandacarubroker.domain.stock.StockRepository;
-import jakarta.validation.*;
+
+import jakarta.validation.ValidatorFactory;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
