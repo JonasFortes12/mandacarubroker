5ab2eb7063c54267fe481e544817e5c22fb1a03f
JonasFortes12 <jonascforte@hotmail.com>
2024-02-08 12:28:43 -0300

    chore: Add javadoc comments on most important classes to understand the API working
    Javadocs play a crucial role in creating readable, self-explanatory, and well-documented code, providing significant benefits in terms of collaboration, maintenance, and code understanding over time.

--- src/main/java/com/mandacarubroker/domain/stock/Stock.java
+++ src/main/java/com/mandacarubroker/domain/stock/Stock.java
@@ -19,19 +19,61 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(of="id")
 public class Stock {
 
+    /**
+     * The unique identifier for the stock.
+     *
+     * This field is annotated with {@code @Id} and {@code @GeneratedValue} to
+     * indicate that it represents the primary key of the stock entity. The
+     * {@code GenerationType.UUID} strategy is used to generate a unique UUID as
+     * the identifier.
+     */
     @Id @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
+    /**
+     * The unique symbol associated with the stock.
+     */
     private String symbol;
+    /**
+     * The name of the company associated with the stock.
+     */
     private String companyName;
+    /**
+     * The current price of the stock.
+     */
     private double price;
 
+    /**
+     * Constructs a new Stock object based on the provided RequestStockDTO.
+     *
+     * This constructor initializes a new Stock object. It extracts the symbol, company name,
+     * and price from the RequestStockDTO and sets the corresponding attributes of the Stock.
+     * Additionally, it calculates the new price using the changePrice method.
+     *
+     * @param requestStockDTO The RequestStockDTO containing data for initializing the Stock.
+     */
     public Stock(final RequestStockDTO requestStockDTO){
         this.symbol = requestStockDTO.symbol();
         this.companyName = requestStockDTO.companyName();
         this.price = changePrice(requestStockDTO.price(), true);
     }
 
+    /**
+     * Changes the price of the stock based on the specified amount and direction.
+     *
+     * This method adjusts the stock price according to the specified amount and
+     * direction (increase or decrease). If the direction is set to increase and
+     * the specified amount is less than the current price, it invokes the
+     * {@code increasePrice} method; otherwise, it invokes the {@code decreasePrice}
+     * method. If the direction is set to decrease and the specified amount is
+     * greater than the current price, it invokes the {@code increasePrice} method;
+     * otherwise, it invokes the {@code decreasePrice} method.
+     *
+     * @param amount The amount by which to adjust the stock price.
+     * @param increase A boolean indicating whether to increase (true)
+     *                 or decrease (false) the price.
+     * @return The adjusted stock price.
+     */
     public double changePrice(final double amount, final boolean increase) {
         if (increase) {
             if (amount < this.price) {
@@ -48,10 +90,22 @@ public class Stock {
         }
     }
 
+    /**
+     * Increases the stock price by the specified amount.
+     *
+     * @param amount The amount by which to increase the stock price.
+     * @return The updated stock price after the increase.
+     */
     public double increasePrice(final double amount) {
         return this.price + amount;
     }
 
+    /**
+     * Decreases the stock price by the specified amount.
+     *
+     * @param amount The amount by which to decrease the stock price.
+     * @return The updated stock price after the decrease.
+     */
     public double decreasePrice(final double amount) {
         return this.price - amount;
     }
--- src/main/java/com/mandacarubroker/service/StockService.java
+++ src/main/java/com/mandacarubroker/service/StockService.java
@@ -19,27 +19,80 @@ import java.util.Set;
 @Service
 public class StockService {
 
-
+    /**
+     * The repository responsible for data access and manipulation of stock entities.
+     */
     private final StockRepository stockRepository;
 
+    /**
+     * Constructs a new instance of the {@link StockService}.
+     *
+     * @param stockRepository The repository for stock entities. Must not be null.
+     */
     public StockService(final StockRepository stockRepository) {
         this.stockRepository = stockRepository;
     }
 
+    /**
+     * Retrieves a list of all available stocks.
+     *
+     * This method delegates the retrieval of stock entities to the associated
+     * {@link StockRepository} by invoking its {@code findAll} method. The returned
+     * list represents all stocks present in the underlying data storage.
+     *
+     * @return A list containing all available stocks.
+     */
     public List<Stock> getAllStocks() {
         return stockRepository.findAll();
     }
 
+    /**
+     * Retrieves a stock by its unique identifier.
+     *
+     * This method delegates the retrieval of a specific stock entity to the associated
+     * {@link StockRepository} by invoking its {@code findById} method with the provided ID.
+     *
+     * @param id The unique identifier of the stock to be retrieved.
+     * @return An {@link Optional} containing the stock with the specified ID if found,
+     *         or an empty {@link Optional} if the stock is not found.
+     */
     public Optional<Stock> getStockById(final String id) {
         return stockRepository.findById(id);
     }
 
+    /**
+     * Creates a new stock based on the provided data.
+     *
+     * This method instantiates a new {@link Stock} object using the data from the
+     * provided {@link RequestStockDTO}. It then validates the data using the
+     * {@code validateRequestStockDTO} method and persists the new stock entity
+     * to the associated {@link StockRepository} using the {@code save} method.
+     *
+     * @param data The data representing the new stock to be created.
+     * @return The created stock entity.
+     * @throws IllegalArgumentException If the provided data is not valid.
+     */
     public Stock createStock(final RequestStockDTO data) {
         Stock novaAcao = new Stock(data);
         validateRequestStockDTO(data);
         return stockRepository.save(novaAcao);
     }
 
+    /**
+     * Updates an existing stock with the provided data.
+     *
+     * This method retrieves the existing stock entity from the associated
+     * {@link StockRepository} using the provided ID. If the stock is found, it
+     * updates its attributes with the data from the provided {@link Stock} object.
+     * The method then calculates the new stock price using
+     * the {@code changePrice} method and persists the updated
+     * stock entity back to the repository using the {@code save} method.
+     *
+     * @param id The unique identifier of the stock to be updated.
+     * @param updatedStock The data representing the updated stock.
+     * @return An {@link Optional} containing the updated stock entity if found,
+     *         or an empty {@link Optional} if the stock with the specified ID is not found.
+     */
     public Optional<Stock> updateStock(final String id, final Stock updatedStock) {
         return stockRepository.findById(id)
                 .map(stock -> {
@@ -52,10 +105,31 @@ public class StockService {
                 });
     }
 
+    /**
+     * Deletes a stock by its unique identifier.
+     *
+     * This method removes the stock entity associated with the specified ID from
+     * the underlying data storage by invoking the {@code deleteById} method of the
+     * associated {@link StockRepository}.
+     *
+     * @param id The unique identifier of the stock to be deleted.
+     */
     public void deleteStock(final String id) {
         stockRepository.deleteById(id);
     }
 
+    /**
+     * Validates a RequestStockDTO object using Bean Validation.
+     *
+     * This static method uses the Bean Validation API to validate the provided
+     * {@link RequestStockDTO} object. It checks for constraints specified by annotations
+     * on the fields of the DTO. If validation fails, a {@link ConstraintViolationException}
+     * is thrown, providing details about the validation errors.
+     *
+     * @param data The RequestStockDTO object to be validated.
+     * @throws ConstraintViolationException If the validation of the RequestStockDTO fails,
+     *                                      containing details of the validation errors.
+     */
     public static void validateRequestStockDTO(final RequestStockDTO data) {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         Validator validator = factory.getValidator();
@@ -74,6 +148,18 @@ public class StockService {
         }
     }
 
+    /**
+     * Validates the RequestStockDTO and creates a new stock if validation succeeds.
+     *
+     * This method first validates the provided {@link RequestStockDTO} using the
+     * {@code validateRequestStockDTO} method. If the validation is successful, a
+     * new {@link Stock} object is instantiated using the provided data, and it is
+     * then persisted to the associated {@link StockRepository} using the {@code save} method.
+     *
+     * @param data The RequestStockDTO object containing data for creating a new stock.
+     * @throws ConstraintViolationException If the validation of the RequestStockDTO fails,
+     *                                      containing details of the validation errors.
+     */
     public void validateAndCreateStock(final RequestStockDTO data) {
         validateRequestStockDTO(data);
 
